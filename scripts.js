6957e71d5772d19ef1c595bcd434dc37cf7b604b
gabrielramost <83712300+gabrielramost@users.noreply.github.com>
2023-08-05 13:59:05 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
 const mobileMenu = document.getElementById("mobile-menu");
 const navbarMenu = document.getElementById("navbar-menu");
+const dropdowns = document.querySelectorAll(".menu-dropdown");
 
 mobileMenu.addEventListener("click", () => {
     navbarMenu.classList.toggle("show");
 });
+
+// Agregar evento de clic para submenús en móviles
+if (window.innerWidth <= 768) {
+    dropdowns.forEach((dropdown) => {
+        dropdown.addEventListener("click", (e) => {
+            e.preventDefault();
+            dropdown.querySelector(".sub-menu").classList.toggle("show");
+        });
+    });
+}
